ce4fd728b7888bb7be478f09c212a81a7d04e2de
brent <brent.e.meyer@gmail.com>
2022-01-20 22:51:44 -0500

    adds swipe to go back extension for custom buttons

--- Gradient/GradientApp.swift
+++ Gradient/GradientApp.swift
@@ -15,3 +15,14 @@ struct GradientApp: App {
         }
     }
 }
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
