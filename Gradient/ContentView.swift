9dc1a58545e6b9d79e23bb931cd217f055523daf
brent <brent.e.meyer@gmail.com>
2022-03-08 23:07:24 -0500

    adds time, dupe logic to log entry button
    - log entry button does not show if an entry exists today
    - users are shown a warning when attempting to log and entry early in
      the day
    
    closes #5

--- Gradient/ContentView.swift
+++ Gradient/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
 
     @State private var showingAddSentimentSheet = false
     @State private var showingSettings = false
+    @State private var showingWarning = false
 
     var entriesByMonth: [[Entry]] {
         guard !entries.isEmpty else { return [] }
@@ -163,33 +164,42 @@ struct ContentView: View {
                                     .padding()
                             }
                         }
+
+                        Spacer(minLength: 64)
                     }
-                    .padding(.bottom, 64)
                     .navigationTitle("Home")
                     .navigationBarHidden(true)
                 }
 
-                VStack {
-                    Spacer()
+                if !Calendar.current.isDateInToday(entries[0].wrappedDate) {
+                    VStack {
+                        Spacer()
 
-                    Button {
-                        showingAddSentimentSheet = true
-                    } label: {
-                        Text("Log day")
-                            .font(.system(size: 20))
-                            .fontWeight(.bold)
-                            .foregroundColor(.black)
-                            .frame(maxWidth: .infinity)
-                            .contentShape(Rectangle())
+                        Button {
+                            let warningTimeCutoff = Calendar.current.date(from: DateComponents(hour: 20)) ?? Date.now
+
+                            if Date.now < warningTimeCutoff {
+                                showingWarning = true
+                            } else {
+                                showingAddSentimentSheet = true
+                            }
+                        } label: {
+                            Text("Log day")
+                                .font(.system(size: 20))
+                                .fontWeight(.bold)
+                                .foregroundColor(.black)
+                                .frame(maxWidth: .infinity)
+                                .contentShape(Rectangle())
+                        }
+                        .padding()
+                        .buttonStyle(PlainButtonStyle())
+                        .frame(maxWidth: .infinity, minHeight: 44, maxHeight: 60)
+                        .background(.white)
+                        .cornerRadius(8)
+                        .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.25), radius: 16, x: 0, y: 8)
                     }
-                    .padding()
-                    .buttonStyle(PlainButtonStyle())
-                    .frame(maxWidth: .infinity, minHeight: 44, maxHeight: 60)
-                    .background(.white)
-                    .cornerRadius(8)
-                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.25), radius: 16, x: 0, y: 8)
+                    .padding(.horizontal)
                 }
-                .padding(.horizontal)
             }
         }
         .sheet(isPresented: $showingAddSentimentSheet) {
@@ -197,6 +207,20 @@ struct ContentView: View {
         }
         .sheet(isPresented: $showingSettings) {
             SettingsView()
+        }
+        .alert("Are you sure?", isPresented: $showingWarning) {
+            Button {
+                showingAddSentimentSheet = true
+            } label: {
+                Text("Yes")
+            }
+
+            Button {
+            } label: {
+                Text("Nevermind")
+            }
+        } message: {
+            Text("We recommend logging your day when it's just about done and it looks like you might have some day left.")
         }
     }
 }
