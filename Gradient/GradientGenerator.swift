df57e8686544a26a6e7d7abd254c74cb7009dcb8
brent <brent.e.meyer@gmail.com>
2022-04-03 16:24:43 -0400

    wip, adds hsb red-to-green gradient

--- Gradient/GradientGenerator.swift
+++ Gradient/GradientGenerator.swift
@@ -9,7 +9,11 @@ import Foundation
 import SwiftUI
 
 struct GradientGenerator {
-    private struct ColorComponents {
+    enum ColorSpace {
+        case rgb, hsb
+    }
+
+    private struct RGBColorComponents {
         let red: Double
         let green: Double
         let blue: Double
@@ -35,6 +39,32 @@ struct GradientGenerator {
         }
     }
 
+    private struct HSBColorComponents {
+        let hue: Double
+        let saturation: Double
+        let brightness: Double
+
+        init(from color: Color) {
+            let uiColor = UIColor(color)
+
+            var h: CGFloat = 0
+            var s: CGFloat = 0
+            var b: CGFloat = 0
+            var a: CGFloat = 0
+
+            guard uiColor.getHue(&h, saturation: &s, brightness: &b, alpha: &a) else {
+                hue = 0
+                saturation = 0
+                brightness = 0
+                return
+            }
+
+            hue = h
+            saturation = s
+            brightness = s
+        }
+    }
+
     let startColor: Color
     let endColor: Color
 
@@ -44,35 +74,85 @@ struct GradientGenerator {
     }
 
     init() {
-        self.startColor = Color(red: 224/255, green: 175/255, blue: 48/255, opacity: 1)
-        self.endColor = Color(red: 64/255, green: 173/255, blue: 126/255, opacity: 1)
+        self.startColor = Color(
+            hue: 0/360,
+            saturation: 75/100,
+            brightness: 75/100
+        )
+
+        self.endColor = Color(
+            hue: 90/360,
+            saturation: 75/100,
+            brightness: 75/100
+        )
     }
 
     func generate(steps: Int) -> [Color] {
-        let startColorComponents = ColorComponents(from: self.startColor)
-        let endColorComponents = ColorComponents(from: self.endColor)
-
-        let redColorSteps = getColorSteps(startValue: startColorComponents.red, endValue: endColorComponents.red, steps: steps)
-        let greenColorSteps = getColorSteps(startValue: startColorComponents.green, endValue: endColorComponents.green, steps: steps)
-        let blueColorSteps = getColorSteps(startValue: startColorComponents.blue, endValue: endColorComponents.blue, steps: steps)
-
         var gradientColors = [Color]()
+        let colorSpace: ColorSpace = .hsb
 
-        for i in 0...(steps - 1) {
-            gradientColors.append(Color(red: redColorSteps[i], green: greenColorSteps[i], blue: blueColorSteps[i], opacity: 1))
+        if colorSpace == .rgb {
+            let startColorComponents = RGBColorComponents(from: self.startColor)
+            let endColorComponents = RGBColorComponents(from: self.endColor)
+
+            let redColorSteps = getValueSteps(startValue: startColorComponents.red, endValue: endColorComponents.red, steps: steps)
+            let greenColorSteps = getValueSteps(startValue: startColorComponents.green, endValue: endColorComponents.green, steps: steps)
+            let blueColorSteps = getValueSteps(startValue: startColorComponents.blue, endValue: endColorComponents.blue, steps: steps)
+
+            for i in 0...(steps - 1) {
+                gradientColors.append(Color(red: redColorSteps[i], green: greenColorSteps[i], blue: blueColorSteps[i], opacity: 1))
+            }
+        } else if colorSpace == .hsb {
+            let startColorComponents = HSBColorComponents(from: self.startColor)
+            let middleColorComponents = HSBColorComponents(from: Color(
+                hue: 55/360, saturation: 75/100, brightness: 75/100
+            ))
+            let endColorComponents = HSBColorComponents(from: self.endColor)
+
+            let firstHalfHueSteps = getValueSteps(startValue: startColorComponents.hue, endValue: middleColorComponents.hue, steps: 50 - 2)
+            let firstHalfSaturationSteps = getValueSteps(startValue: startColorComponents.saturation, endValue: middleColorComponents.saturation, steps: 50 - 2)
+            let firstHalfBrightnessSteps = getValueSteps(startValue: startColorComponents.brightness, endValue: middleColorComponents.brightness, steps: 50 - 2)
+
+            let secondHalfHueSteps = getValueSteps(startValue: middleColorComponents.hue, endValue: endColorComponents.hue, steps: 50 - 2)
+            let secondHalfSaturationSteps = getValueSteps(startValue: middleColorComponents.saturation, endValue: endColorComponents.saturation, steps: 50 - 2)
+            let secondHalfBrightnessSteps = getValueSteps(startValue: middleColorComponents.brightness, endValue: endColorComponents.brightness, steps: 50 - 2)
+
+            for i in 0...((steps - 1) / 2) {
+                gradientColors.append(Color(hue: firstHalfHueSteps[i], saturation: firstHalfSaturationSteps[i], brightness: firstHalfBrightnessSteps[i]))
+            }
+
+            for i in 0...((steps - 1) / 2) {
+                gradientColors.append(Color(hue: secondHalfHueSteps[i], saturation: secondHalfSaturationSteps[i], brightness: secondHalfBrightnessSteps[i]))
+            }
+
+            /*
+            let hueColorSteps = getValueSteps(startValue: startColorComponents.hue, endValue: endColorComponents.hue, steps: steps)
+            let saturationColorSteps = getValueSteps(startValue: startColorComponents.saturation, endValue: endColorComponents.saturation, steps: steps)
+            let brightnessColorSteps = getValueSteps(startValue: startColorComponents.brightness, endValue: endColorComponents.brightness, steps: steps)
+
+            for i in 0...(steps - 1) {
+                gradientColors.append(Color(hue: hueColorSteps[i], saturation: saturationColorSteps[i], brightness: brightnessColorSteps[i], opacity: 1))
+            }
+            */
         }
 
         return gradientColors
     }
 
-    private func getColorSteps(startValue: Double, endValue: Double, steps: Int) -> [Double] {
-        let stepIncrement = (endValue - startValue) / Double(steps)
-        let middleSteps = steps - 2
+    private func getValueSteps(startValue: Double, endValue: Double, steps: Int) -> [Double] {
+        let stepIncrement = abs((startValue - endValue)) / Double(steps)
+        //let middleSteps = steps - 2
 
         var stepValues: [Double] = [startValue]
 
-        for i in 1...middleSteps {
-            let nextColor = startValue + (stepIncrement * Double(i))
+        for i in 0...steps {
+            var nextColor: Double {
+                if startValue < endValue {
+                    return startValue + (stepIncrement * Double(i))
+                } else {
+                    return startValue - (stepIncrement * Double(i))
+                }
+            }
             stepValues.append(nextColor)
         }
 
--- Gradient/NewEntryView.swift
+++ Gradient/NewEntryView.swift
@@ -46,6 +46,13 @@ struct NewEntryView: View {
                 }
 
                 Spacer()
+
+                HStack {
+                    Rectangle()
+                        .foregroundColor(gradientColors[0])
+                    Rectangle()
+                        .foregroundColor(gradientColors[99])
+                }
             }
             .frame(maxWidth: .infinity)
             .background(sentimentColor)
